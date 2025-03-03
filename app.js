d4c9ce5edc60b7ab7fc4d974e92f44c172c4726c
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-03-04 02:05:38 +0500

    code update

--- app.js
+++ app.js
@@ -4,11 +4,27 @@
 
 // ANSWER
 
+/*let currentTime = new Date()
+
+document.write("<h4>Current Time:</h4>" + currentTime)*/
+
 // QUESTION 2
 // Write a program that alerts the current month in words.
 // For example December.
 
 // ANSWER
+
+// Getting current date
+/*let currentDate = new Date()
+
+// Getting month from current date
+let currentMonth = currentDate.getMonth()
+
+// Months array
+const months = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+
+document.write("<h4>Current Month:</h4>" + months[currentMonth]);*/
 
 // QUESTION 3
 // Write a program that alerts the first 3 letters of the current
@@ -17,11 +33,39 @@
 
 // ANSWER
 
+// Getting current time
+/*let currentDay = new Date()
+
+// Getting current day
+let day = currentDay.getDay()
+
+// Days array
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// Making copy
+document.write("<h4>Day today:</h4>" + "Today is " + days[day].slice(0 ,3));*/
+
+
 // QUESTION 4
 // Write a program that displays a message “It’s Fun day” if
 // its Saturday or Sunday today.
 
 // ANSWER
+
+// Getting current time
+/*let currentDay = new Date()
+
+// Getting current day
+let dayToday = new Date(currentDay).getDay()
+
+// Days array
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// Making condition
+if(dayToday === "Saturday" || dayToday === "Sunday"){
+    alert("It's Fun day")
+}
+else(alert("It's not funday"))*/
 
 // QUESTION 5
 // Write a program that shows the message “First fifteen
@@ -30,6 +74,8 @@
 
 // ANSWER
 
+
+
 // QUESTION 6
 // Write a program that determines the minutes since
 // midnight, Jan. 1, 1970 and assigns it to a variable that
@@ -37,6 +83,18 @@
 // to represent the Date object.
 
 // ANSWER
+
+/*let currentToday = new Date()
+
+let millisecPassedSince = currentToday.getTime()
+
+let minutesPassedSince = (millisecPassedSince / (1000 * 60))
+
+document.write("<h4>Current Time:</h4>" + currentToday + "<h4>MilliSeconds Passed since Midnight, Jan 1, 1970:</h4>" + millisecPassedSince
++ "<h4>Minutes Passed since Midnight, Jan 1, 1970:</h4>" + minutesPassedSince) 
+
+console.log(minutesPassedSince);*/
+
 
 // QUESTION 7
 // Write a program that tests whether it's before noon and
@@ -51,10 +109,17 @@
 
 // ANSWER
 
+/*let todayDate = new Date()
+
+let laterDate = new Date("31 December 2020")
+
+document.write("<h4>Later Date:</h4>" + laterDate)*/
+
+
 // QUESTION 9
-// Write a program that creates a Date object for the last day
-// of the last month of 2020 and assigns it to variable named
-// laterDate.
+// Create a date object of the starting date of this Ramadan
+// and alert the number of days past since 1st Ramadan?
+// Note: 1st Ramadan was on June 18, 2015
 
 // ANSWER
 
